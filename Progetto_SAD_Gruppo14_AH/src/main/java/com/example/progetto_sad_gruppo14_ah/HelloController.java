f4955a226cd96186f9a00e72bcd541f212e32025
ColMike1 <michelecolombo218@gmail.com>
2025-05-17 11:49:51 +0200

    Add files via upload

---
+++ Progetto_SAD_Gruppo14_AH/src/main/java/com/example/progetto_sad_gruppo14_ah/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.progetto_sad_gruppo14_ah;
+
+import javafx.fxml.FXML;
+
+public class HelloController{
+
+    @FXML
+    private void initialize() {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ Progetto_SAD_Gruppo14_AH/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.progetto_sad_gruppo14_ah {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.progetto_sad_gruppo14_ah to javafx.fxml;
+    exports com.example.progetto_sad_gruppo14_ah;
+}
