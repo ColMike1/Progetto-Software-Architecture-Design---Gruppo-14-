c15527321ed30a293ea811bd825e0e2b1f8ba348
ColMike1 <michelecolombo218@gmail.com>
2025-05-17 14:51:44 +0200

    Selezione del bottone_figura dalla toolbar è stato implementato con il pattern Factory Method

---
+++ Progetto_SAD_Gruppo14_AH/src/main/java/com/example/Factory/FiguraFactory.java
@@ -0,0 +1,7 @@
+package com.example.Factory;
+public interface FiguraFactory {
+
+    // public Figura creaFigura(double x1, double y1, double x2, double y2);
+
+    /* Bisogna creare la classe Figura per poter utilizzare il metodo creaFigura*/
+}
---
+++ Progetto_SAD_Gruppo14_AH/src/main/java/com/example/Factory/RettangoloFactory.java
@@ -0,0 +1,3 @@
+package com.example.Factory;
+public class RettangoloFactory implements FiguraFactory {
+}
